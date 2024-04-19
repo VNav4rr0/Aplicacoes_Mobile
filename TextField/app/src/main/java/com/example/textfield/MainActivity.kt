16511d5fb8b17ab6d246ea68018c89a1c32cdc22
VNav4rr0 <vn120107@gmail.com>
2024-04-19 18:09:10 -0300

    add arquivos

---
+++ TextField/app/src/main/java/com/example/textfield/MainActivity.kt
@@ -0,0 +1,41 @@
+package com.example.textfield
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.textfield.componentes.SimpleTextField
+import com.example.textfield.ui.theme.TextFieldTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            SimpleTextField()
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Column{
+            SimpleTextField()
+    }
+
+}
